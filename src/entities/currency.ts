bbd9fef3e2d660dace14abb72e1ede3b768f114d
icecream-teams <kusama001@outlook.com>
2023-05-04 22:22:04 +0800

    feat: darwinia native token

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'CRAB', 'CRAB')
+  public static readonly ETHER: Currency = new Currency(18, 'RING', 'Darwinia Network Native Token')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
